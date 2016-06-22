ed6cdd99a59f93e9f6f63de0e79aa10f45412077
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-06-22 14:00:43 -0400

    Add code to server.js to initalize the trail database

--- server.js
+++ server.js
@@ -2,8 +2,35 @@
 var express = require('express');
 var path = require('path');
 var sqlite3 = require('sqlite3').verbose();
-var db = require('./database.js');
+var db = new sqlite3.Database('trail.db');
 
+// Initializing SQLite3 database
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='trail'",
+       function(err, rows) {
+  if(err !== null) {
+    console.log(err);
+  }
+  else if(rows === undefined) {
+    db.run('CREATE TABLE "trail" ' +
+           '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+           '"trail_name" VARCHAR(150), ' +
+           '"lat" FLOAT,' +
+           '"long" FLOAT,' +
+           '"description" TEXT,' +
+           '"review" TEXT,' +
+           '"username" VARCHAR(25))', function(err) {
+      if(err !== null) {
+        console.log(err);
+      }
+      else {
+        console.log("SQL Table 'trail' initialized.");
+      }
+    });
+  }
+  else {
+    console.log("SQL Table 'trail' already initialized.");
+  }
+});
 // Declaring other variables
 // The var port is set to list to to the port specified in the ENV variable or on 8080
 var port = process.env.PORT || 8080;
