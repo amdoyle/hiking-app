9246174f12c22b28b783b86916174ef8c5bda5bd
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-07-08 13:22:00 -0400

    Refacter server.js by updating routes to use chaining functions and reduce duplication

--- server.js
+++ server.js
@@ -30,92 +30,90 @@ var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 // parse application/json
 app.use(bodyParser.json())
 
-app.get("/", function(req, res) {
-  return res.sendFile(__dirname + "/views/index.html");
-});
+app.route('/')
+  .get(function(req, res) {
+    return res.sendFile(__dirname + "/views/index.html");
+  })
+  .post(parseUrlencoded, function(req, res, next) {
+    // Escaping harmful characters
+    var name = escape(validator.trim(req.body.trailName));
+    var inputLat = validator.trim(req.body.lat);
+    var inputLong = validator.trim(req.body.long);
+    var descrip = escape(validator.trim(req.body.description));
+    var rev = escape(validator.trim(req.body.review));
+    var user = validator.trim(req.body.username);
 
-app.get('/trails', function(req,res) {
-  // res.sendFile(path.join(__dirname + "/public" + "/index.html"));
-    db.all("SELECT * FROM trail ORDER BY trail_name;", function(err, rows) {
-      if(err != null) {
-        console.log(err);
-        // next(err);
-      } else {
 
-        var unencodedRow;
-        var unencodedRows = [];
-        // Looping through each item and unecoding the none alpha/numerica characters
-        for(var row in rows) {
-          unencodedRow =  {
-            trail_name: unescape(rows[row].trail_name),
-            description: unescape(rows[row].description),
-            review: unescape(rows[row].review),
-            username: unescape(rows[row].username),
-            lat: rows[row].lat,
-            long: rows[row].long
-          }
-          //Each row will be push in to the new array
-          unencodedRows.push(unencodedRow);
+    //setting the object to be returned
+    var newtrail = {
+      trail_name: name,
+      description: descrip,
+      review: rev,
+      username: user,
+      lat: inputLat,
+      long: inputLong
+    }
 
+    //  Checking to ensure that all paramaters meet the validations before saving
+    if(validator.isAlphanumeric(user) && validator.isAscii(name, descrip, rev)
+    && validator.isFloat(inputLat) && validator.isFloat(inputLat) &&
+    !validator.isNull(user, name, descrip, rev, inputLong, inputLat))  {
+      // SQL statment to input the info
+      var sqlRequest = "INSERT INTO TRAIL (trail_name, lat, long, description, review, username)" +
+                       "VALUES ('" + name + "', '" + inputLat + "', '" + inputLong + "', '" + descrip
+                       + "', '" + rev + "', '" + user + "')";
+
+      // telling the database to run the statment
+      db.run(sqlRequest, function(err) {
+        if(err){
+          console.log(err);
+          next(err);
+            res.status(400).json("Sorry, something went wrong. Please try again.");
         }
 
-        // Sending the unencoded array to the view
-        res.send(unencodedRows);
 
-      }
+        //  res.send("Success your trail has been saved");
+        res.status(201).json(newtrail);
 
-      console.log(db.each('SELECT * FROM trail'));
-    });
-});
+      });
 
-app.post("/", parseUrlencoded, function(req, res, next) {
-  // Escaping harmful characters
-  var name = escape(validator.trim(req.body.trailName));
-  var inputLat = validator.trim(req.body.lat);
-  var inputLong = validator.trim(req.body.long);
-  var descrip = escape(validator.trim(req.body.description));
-  var rev = escape(validator.trim(req.body.review));
-  var user = validator.trim(req.body.username);
+    } else {
+      res.status(400).json('Invalid input. Please try again.');
+    }
 
+  });
 
-  //setting the object to be returned
-  var newtrail = {
-    trail_name: name,
-    description: descrip,
-    review: rev,
-    username: user,
-    lat: inputLat,
-    long: inputLong
-  }
+app.route('/trails')
+  .get(function(req,res) {
+    // res.sendFile(path.join(__dirname + "/public" + "/index.html"));
+      db.all("SELECT * FROM trail ORDER BY trail_name;", function(err, rows) {
+        if(err != null) {
+          console.log(err);
+          // next(err);
+        } else {
 
-  //  Checking to ensure that all paramaters meet the validations before saving
-  if(validator.isAlphanumeric(user) && validator.isAscii(name, descrip, rev)
-  && validator.isFloat(inputLat) && validator.isFloat(inputLat) &&
-  !validator.isNull(user, name, descrip, rev, inputLong, inputLat))  {
-    // SQL statment to input the info
-    var sqlRequest = "INSERT INTO TRAIL (trail_name, lat, long, description, review, username)" +
-                     "VALUES ('" + name + "', '" + inputLat + "', '" + inputLong + "', '" + descrip
-                     + "', '" + rev + "', '" + user + "')";
+          var unencodedRow;
+          var unencodedRows = [];
+          // Looping through each item and unecoding the none alpha/numerica characters
+          for(var row in rows) {
+            unencodedRow =  {
+              trail_name: unescape(rows[row].trail_name),
+              description: unescape(rows[row].description),
+              review: unescape(rows[row].review),
+              username: unescape(rows[row].username),
+              lat: rows[row].lat,
+              long: rows[row].long
+            }
+            //Each row will be push in to the new array
+            unencodedRows.push(unencodedRow);
 
-    // telling the database to run the statment
-    db.run(sqlRequest, function(err) {
-      if(err){
-        console.log(err);
-        next(err);
-          res.status(400).json("Sorry, something went wrong. Please try again.");
-      }
+          }
+          // Sending the unencoded array to the view
+          res.send(unencodedRows);
 
-
-      //  res.send("Success your trail has been saved");
-      res.status(201).json(newtrail);
-
-    });
-
-  } else {
-    res.status(400).json('Invalid input. Please try again.');
-  }
-
-});
+        }
+      });
+  });
 
 // db.close();
 
